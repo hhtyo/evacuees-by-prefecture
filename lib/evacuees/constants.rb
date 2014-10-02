48e7b28259dbe6226b7591e602a0dc87bb1032f2
Soren Jones <soren.jones@gmail.com>
2014-10-02 19:20:51 +0900

    Add 2014-09-11 data

--- lib/evacuees/constants.rb
+++ lib/evacuees/constants.rb
@@ -62,7 +62,8 @@ module Evacuees
         'http://www.reconstruction.go.jp/topics/main-cat2/sub-cat2-1/20140523_hinansha.pdf',
         'http://www.reconstruction.go.jp/topics/main-cat2/sub-cat2-1/20140624_hinansha.pdf',
         'http://www.reconstruction.go.jp/topics/main-cat2/sub-cat2-1/20140729_hinansha.pdf',
-        'http://www.reconstruction.go.jp/topics/main-cat2/sub-cat2-1/20140829_hinansha.pdf'
+        'http://www.reconstruction.go.jp/topics/main-cat2/sub-cat2-1/20140829_hinansha.pdf',
+        'http://www.reconstruction.go.jp/topics/main-cat2/sub-cat2-1/20140930_hinansha.pdf'
       ]
     end
 
